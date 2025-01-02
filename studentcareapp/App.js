222fe851a7b16e74bffea9f3ffad7c41f99040be
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2025-01-02 19:35:17 +0000

    add header path

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './Components/Header'
+import Header from './Components/Header';
 
 export default function App() {
   return (
     <View>
       <Header></Header>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
--- studentcareapp/Components/Header.js
+++ studentcareapp/Components/Header.js
@@ -1,29 +1,54 @@
 import React from 'react';
-import { View,Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView ,} from 'react-native';
 
-
-const Header = () => {
+export default function App() {
+   
     return (
-        <View style={styles.header}>
-            <Text style={styles.header_text}>UoV Student Care</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            {/* Header */}
+            <View style={styles.header_text}>
+                <Text style={styles.header_text_content}>UoV Student Care</Text>
+            </View>
+            
+            {/* Image */}
+            <View style={styles.imageContainer}>
+            <Image 
+                source={require('../assets/Logo/UoV_Logo.png')}
+                style={styles.image}
+            />
+            </View>
+            
+        </ScrollView>
     );
-};
+}
 
 const styles = StyleSheet.create({
-    header: {
-        backgroundColor: '#4b0150',
-        alignItems: 'center',
-        width: '1140px',
-        height: '150px',
-
-
+    container: {
+        flexGrow: 1,
+        alignItems: 'stretch',
+        backgroundColor: '#fff',
     },
     header_text: {
+        backgroundColor: '#4b0150',
+        maxWidth: '100%',
+        height:80,
+        justifyContent: 'center',
+    },
+    header_text_content: {
         color: '#fff',
-        fontSize: '40px',
-        marginTop: '70px'
-    }
+        fontSize: 15,
+        textAlign: 'center',
+        marginTop:30
+    },
+    imageContainer: {
+       alignItems: 'center', 
+       maxWidth: '100%',
+      
+       
+    },
+    image: {
+        width: 150, 
+        height: 50, 
+        resizeMode: 'contain', 
+    },
 });
-
-export default Header;
