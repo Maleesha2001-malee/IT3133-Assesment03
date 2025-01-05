7ffaf200fccf4abeff4a066986d6050f43cbe7bc
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2025-01-05 14:11:14 +0530

    Update Profile.js

--- studentcareapp/Components/Profile.js
+++ studentcareapp/Components/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
-const Course = ({ studentData }) => {
+const Profile = ({ studentData }) => {
   return (
     <>
       <View style={styles.profile}>
@@ -143,4 +143,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Course;
+export default Profile;
